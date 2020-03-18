a0e3b6980c5278b8f3ee43aa4b2f80524e6d4e8e
Magdalena Kosiorek <magdalenakosiorek96@gmail.com>
2020-03-18 14:05:13 +0100

    editing styles

--- js/newPage.js
+++ js/newPage.js
@@ -2,7 +2,7 @@ function myFunction() {
     var elmnt = document.getElementById("paper");
     var form = document.getElementById("f1");
     var cln = elmnt.cloneNode(true);
-    cln.class ="paper";
+    cln.class ="paper"
     cln.textContent="lal";
     //cln.after("paper");
     document.body.append(cln);
